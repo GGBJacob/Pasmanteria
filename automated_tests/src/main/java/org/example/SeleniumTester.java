ed316740a4bb0994530b662447671d4524f842cf
Jacob <j.romanowski133@gmail.com>
2024-11-06 19:28:10 +0100

    First automated automated tests.
    - Created java selenium project
    - Created SeleniumTester Class
    - Created multiple functions for further evaluation with @IVOBOT

---
+++ automated_tests/src/main/java/org/example/SeleniumTester.java
@@ -0,0 +1,144 @@
+package org.example;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+import java.util.ArrayList;
+import java.util.List;
+
+public class SeleniumTester {
+
+    private final WebDriver driver;
+
+    public SeleniumTester() {
+        WebDriverManager.chromedriver().setup();
+        this.driver = new ChromeDriver();
+    }
+
+    public void testSignUp()
+    {
+        try{
+            driver.get("http://localhost:8080/en/login?create_account");
+
+            // Register new user
+            WebElement genderRadioButton = driver.findElement(By.id("field-id_gender-1"));
+            WebElement firstNameField = driver.findElement(By.name("field-firstname"));
+            WebElement lastNameField = driver.findElement(By.name("field-lastname"));
+            WebElement emailField = driver.findElement(By.id("field-email"));
+            WebElement passwordField = driver.findElement(By.id("field-password"));
+            WebElement loginButton = driver.findElement(By.id("submit-login"));
+            WebElement termsAndConditionsCheckbox = driver.findElement(By.name("psgdpr"));
+
+            genderRadioButton.click();
+            firstNameField.sendKeys("John");
+            lastNameField.sendKeys("Smith");
+            emailField.sendKeys("user@gmail.com");
+            passwordField.sendKeys("secretPassword123");
+            if(!termsAndConditionsCheckbox.isSelected())
+                termsAndConditionsCheckbox.click();
+            loginButton.click();
+
+            // Set driver wait time for expected elements
+            WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(5));
+            WebElement loggedInElement = wait.until(
+                    ExpectedConditions.presenceOfElementLocated(By.id("logged-in-element")  // TODO: Find apropriate ID
+                    ));
+
+
+            if (loggedInElement.isDisplayed()) {
+                System.out.println("Successfully registered!");
+            } else {
+                System.out.println("Failed to register!");
+            }
+        }
+        catch(Exception e){
+            e.printStackTrace();
+        }
+    }
+
+    public void testProductAdding()
+    {
+        try{
+        driver.get("http://localhost:8080/en/");
+
+        List<WebElement> categories = driver.findElements(By.cssSelector("ul#top-menu > li.category a.dropdown-submenu"));
+
+        List<String> categoryUrls = new ArrayList<>();
+
+        for (WebElement category : categories)
+        {
+            categoryUrls.add(category.getAttribute("href"));
+        }
+        int addedProducts = 0;
+        for (int i =0; i<categoryUrls.size() && addedProducts < 10; i++) {
+
+            driver.get(categoryUrls.get(i));
+
+            WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(5)); // Component waiting x seconds before returning an error
+
+            List<WebElement> productList = driver.findElements(By.cssSelector("a.product-thumbnail"));
+
+            for (int j =0; j<productList.size() && addedProducts < 10; j++) {
+
+                // Open product page
+                wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("a.product-thumbnail")));
+                productList = driver.findElements(By.cssSelector("a.product-thumbnail")); // TODO: Ogarnac co tu
+                WebElement product = productList.get(j);
+                product.click();
+
+                // Wait for add to cart button to load
+                WebElement addToCartButton = wait.until(ExpectedConditions.presenceOfElementLocated(By.className("add-to-cart")));
+                addToCartButton.click();
+
+                // Wait for continue button to load
+                WebElement continueButton = wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector(".cart-content-btn .btn-secondary")));
+                continueButton.click();
+
+                // Return to category page
+                driver.get(categoryUrls.get(i));
+
+                // Increment added product count
+                addedProducts++;
+            }
+        }}
+        catch(Exception e){
+            e.printStackTrace();
+        }
+
+        // TODO: Add 10 products (different counts) from different categories to cart
+    }
+
+    public void testProductSearch()
+    {
+        // TODO: Search for a product and add random from result
+    }
+
+    public void testProductRemoval()
+    {
+        // TODO: Remove 3 products from cart
+    }
+
+    public void testOrder()
+    {
+        // TODO: Order, select payment method (on delivery), select one form of shipping @IVOBOT
+    }
+
+    public void testOrderStatus()
+    {
+        // TODO: Check order status @IVOBOT
+    }
+
+    public void testInvoice()
+    {
+        // TODO: Generate an invoice @IVOBOT
+    }
+
+
+}
---
+++ automated_tests/src/main/java/org/example/Tester.java
@@ -0,0 +1,9 @@
+package org.example;
+
+public class Tester {
+    public static void main(String[] args) {
+        SeleniumTester tester = new SeleniumTester();
+        // tester.testSignUp();
+        tester.testProductAdding();
+    }
+}
