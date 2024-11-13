1d06c26d13c7663da254816e69e4609f21576191
Karol Zwierz <karoljzwierz@gmail.com>
2024-11-13 20:46:47 +0100

    Src fix (DB version was off)
    IMPORTANT: Remember about doing a backup of the database right before the COMMIT and PUSH in order to save DB changes (regardless of whether you manually changed something in the DB), and doing a restore right after the PULL in order to have a correct database version as it is NOT directly versioned (only a dump is). If DB scripts do not work, try chmod. 'install' directory removed after installation.

--- src/var/cache/dev/smarty/compile/classic/a9/d4/96/a9d496f00a4f81df42699c4952850153d0f01d26_0.module.psmboviewstemplateshookre.php
+++ src/var/cache/dev/smarty/compile/classic/a9/d4/96/a9d496f00a4f81df42699c4952850153d0f01d26_0.module.psmboviewstemplateshookre.php
@@ -1,18 +1,18 @@
 <?php
-/* Smarty version 3.1.48, created on 2024-11-09 11:29:45
+/* Smarty version 3.1.48, created on 2024-11-13 20:44:45
   from 'module:psmboviewstemplateshookre' */
 
 /* @var Smarty_Internal_Template $_smarty_tpl */
 if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
   'version' => '3.1.48',
-  'unifunc' => 'content_672f3999521e22_81708181',
+  'unifunc' => 'content_673501ad1d1008_10640396',
   'has_nocache_code' => false,
   'file_dependency' => 
   array (
     'a9d496f00a4f81df42699c4952850153d0f01d26' => 
     array (
       0 => 'module:psmboviewstemplateshookre',
-      1 => 1730972237,
+      1 => 1731526766,
       2 => 'module',
     ),
   ),
@@ -20,7 +20,7 @@ if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
   array (
   ),
 ),false)) {
-function content_672f3999521e22_81708181 (Smarty_Internal_Template $_smarty_tpl) {
+function content_673501ad1d1008_10640396 (Smarty_Internal_Template $_smarty_tpl) {
 ?><!-- begin /var/www/html/modules/ps_mbo/views/templates/hook/recommended-modules.tpl -->
 <?php echo '<script'; ?>
 >
